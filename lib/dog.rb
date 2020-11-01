21289a0751d6c7d0b04383c269b3658e238ffcb2
Christie Yoon <cleex1999@gmail.com>
2020-11-01 03:35:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,11 +79,12 @@ class Dog
     def self.find_or_create_by(name:, breed:)
         dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ? LIMIT 1", name, breed)
         if !dog.empty?
-            row = dog[0]
-            self.new_from_db(row)
-        else
-            self.create(name:name, breed: breed)
-        end
+     dog_data = dog[0]
+     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+   else
+     dog = self.create(name: name, breed: breed)
+   end
+   dog
     end
 
     def update
